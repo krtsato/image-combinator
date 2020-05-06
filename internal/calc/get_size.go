eeba6afa47ddbc888c532fdb73ddfe5a669a3d18
krtsato <ev60596@meiji.ac.jp>
2020-05-06 23:43:28 +0900

    feat: horizontal image combination

--- internal/calc/get_size.go
+++ internal/calc/get_size.go
@@ -15,6 +15,25 @@ func MaxWidth(imgs input.Images) int {
 	return result
 }
 
+func MaxHeight(imgs input.Images) int {
+	var result int
+	for _, img := range imgs {
+		if result < img.Height {
+			result = img.Height
+		}
+	}
+
+	return result
+}
+func SumWidth(imgs input.Images) int {
+	var result int
+	for _, img := range imgs {
+		result += img.Width
+	}
+
+	return result
+}
+
 func SumHeight(imgs input.Images) int {
 	var result int
 	for _, img := range imgs {
--- internal/convert/combine.go
+++ internal/convert/combine.go
@@ -9,16 +9,16 @@ import (
 
 func Combine(imgs input.Images) *image.RGBA {
 	// 背景画像の作成
-	outImgWidth := calc.MaxWidth(imgs)
-	outImgHeight := calc.SumHeight(imgs)
+	outImgWidth := calc.SumWidth(imgs)
+	outImgHeight := calc.MaxHeight(imgs)
 	outImg := image.NewRGBA(image.Rect(0, 0, outImgWidth, outImgHeight))
 
 	// 背景画像への書き込み
-	pos := 0
+	posX, posY := 0, 0
 	for _, img := range imgs {
-		rect := image.Rect(0, pos, img.Width, pos + img.Height)
+		rect := image.Rect(posX, posY, posX + img.Width, posY + img.Height)
 		draw.Draw(outImg, rect, img.Src, image.Point{0, 0}, draw.Over)
-		pos += img.Height
+		posX += img.Width
 	}
 
 	return outImg
