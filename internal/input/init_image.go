3169607693be8565b653c394aaeed3cce35aeae4
krtsato <ev60596@meiji.ac.jp>
2020-05-07 00:33:50 +0900

    refactor: file open redundancy

--- internal/input/init_image.go
+++ internal/input/init_image.go
@@ -14,13 +14,7 @@ type Image struct {
 type Images []Image
 
 // 画像データの読み込み
-func getImage(path string) (image.Image, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
+func getImage(file *os.File) (image.Image, error) {
 	img, _, err := image.Decode(file)
 	if err != nil {
 		return nil, err
@@ -30,13 +24,7 @@ func getImage(path string) (image.Image, error) {
 }
 
 // 画像サイズの読み込み
-func getImageConfig(path string) (image.Config, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return image.Config{}, err
-	}
-	defer file.Close()
-
+func getImageConfig(file *os.File) (image.Config, error) {
 	config, _, err := image.DecodeConfig(file)
 	if err != nil {
 		return image.Config{}, err
@@ -47,17 +35,21 @@ func getImageConfig(path string) (image.Config, error) {
 
 // Image 構造体の初期化
 func InitImage(path string) (Image, error) {
-	img, err := getImage(path)
+	file, err := os.Open(path)
+	if err != nil {
+		return Image{}, err
+	}
+	defer file.Close()
+
+	img, err := getImage(file)
 	if err != nil {
 		return Image{}, err
 	}
 
-	config, err := getImageConfig(path)
+	config, err := getImageConfig(file)
 	if err != nil {
 		return Image{}, err
 	}
 
 	return Image{img, config.Width, config.Height}, nil
 }
-
-
